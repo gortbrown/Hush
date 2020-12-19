2be39e4978833b206568bd661d922a5b041a2139
gman2 <gjbrown@protonmail.com>
2020-12-19 02:13:06 -0600

    Add project files.

---
+++ HashTerm/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HashTerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxExecButton = new System.Windows.Forms.Button();
+            this.uxCmdInput = new System.Windows.Forms.TextBox();
+            this.uxWebView = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // uxExecButton
+            // 
+            this.uxExecButton.Location = new System.Drawing.Point(742, 8);
+            this.uxExecButton.Name = "uxExecButton";
+            this.uxExecButton.Size = new System.Drawing.Size(46, 20);
+            this.uxExecButton.TabIndex = 0;
+            this.uxExecButton.Text = "-->";
+            this.uxExecButton.UseVisualStyleBackColor = true;
+            this.uxExecButton.Click += new System.EventHandler(this.uxExecButton_Click);
+            // 
+            // uxCmdInput
+            // 
+            this.uxCmdInput.Location = new System.Drawing.Point(12, 8);
+            this.uxCmdInput.Name = "uxCmdInput";
+            this.uxCmdInput.Size = new System.Drawing.Size(724, 20);
+            this.uxCmdInput.TabIndex = 1;
+            // 
+            // uxWebView
+            // 
+            this.uxWebView.Location = new System.Drawing.Point(12, 35);
+            this.uxWebView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.uxWebView.Name = "uxWebView";
+            this.uxWebView.Size = new System.Drawing.Size(775, 403);
+            this.uxWebView.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uxWebView);
+            this.Controls.Add(this.uxCmdInput);
+            this.Controls.Add(this.uxExecButton);
+            this.Name = "Form1";
+            this.Text = "Browse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxExecButton;
+        private System.Windows.Forms.TextBox uxCmdInput;
+        private System.Windows.Forms.WebBrowser uxWebView;
+    }
+}
+
---
+++ HashTerm/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace HashTerm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void uxExecButton_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo command = new ProcessStartInfo() { FileName = "/bin/bash", Arguments = uxCmdInput.Text + " | tee ~/output.txt" };
+            Process p = new Process() { StartInfo = command, };
+            p.Start();
+            string cmdOutput;
+            using (StreamReader readIn = new StreamReader("~/output.txt"))
+            {
+                cmdOutput = readIn.ReadToEnd();
+                readIn.Close();
+            }
+            using (StreamWriter writeOut = File.AppendText("~/output.html"))
+            { 
+                writeOut.WriteLine("<html>");
+                writeOut.WriteLine("<head>");
+                writeOut.WriteLine("<title> Editing document.docx </title>");
+                writeOut.WriteLine("</head>");
+                writeOut.WriteLine("<body>");
+                writeOut.WriteLine("<h1> document.docx </h1>");
+                writeOut.Write("<input type=text value=");
+                writeOut.Write(cmdOutput);
+                writeOut.WriteLine(">");
+                writeOut.WriteLine("</body>");
+                writeOut.WriteLine("</html>");
+                writeOut.Close();
+            }
+            uxWebView.Url = new Uri("~/output.html");
+        }
+    }
+}
